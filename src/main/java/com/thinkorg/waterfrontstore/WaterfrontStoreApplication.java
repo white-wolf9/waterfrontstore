8fb8c39ca85b37ae8b60a3661f416a184b797b75
Saurav Sanyal <sanyal.s271@gmail.com>
2020-10-31 18:00:58 +0530

    Initial commit

---
+++ src/main/java/com/thinkorg/waterfrontstore/WaterfrontStoreApplication.java
@@ -0,0 +1,13 @@
+package com.thinkorg.waterfrontstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterfrontStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterfrontStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkorg/waterfrontstore/WaterfrontStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkorg.waterfrontstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterfrontStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
